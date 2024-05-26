4b0a3c5cc922dedbb2da5e6ff17d9c5cb15cb37f
yosih98 <yosih1998@gmail.com>
2024-05-26 21:51:04 +0800

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,12 +1,12 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // تنقية والتحقق من صحة البيانات المدخلة
+    // Sanitize and validate input data
     $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
     $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $to = "yosih1998@gmail.com"; // استبدل هذا البريد الإلكتروني بالبريد الذي تريد استقبال الرسائل عليه
+        $to = "yosih1998@gmail.com"; // Replace with your email
         $subject = "New Message from Website Contact Form";
         $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
         $headers = "From: $email";
